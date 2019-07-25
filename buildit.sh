d933d42ec77d70e9fde95c4c80fc1858121d6781
Patrick Barone <psb@knightscope.com>
2019-07-25 13:50:54 -0700

    Adding catkin_make_isolated

--- buildit.sh
+++ buildit.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 source /opt/ros/kinetic/setup.bash
 cd /code
-catkin_make
-catkin_make install
+if [ "$MAKE_ISOLATED" = "true" ]; then
+    exit 1
+    catkin_make_isolated
+    catkin_make_isolated install
+else
+    catkin_make
+    catkin_make install
+fi
