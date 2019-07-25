5dd0920af99759b828d2bc7fd02da35e1bcf8a60
Patrick Barone <psb@knightscope.com>
2019-07-25 14:16:48 -0700

    No catkin_make_isolated test

--- testit.sh
+++ testit.sh
@@ -6,11 +6,7 @@ cd /code
 if [ "$MAKE_ISOLATED" = "true" ]; then
     echo "MAKING ISOLATED"
     catkin_make_isolated
-    catkin_make_isolated test
-    catkin_make_isolated install
 else
     echo "MAKING NOT ISOLATED"
-    catkin_make
     catkin_make test
-    catkin_make install
 fi
