6ffc9c454dbb29c679301a8afc065f0b267db5d8
Patrick Barone <psb@knightscope.com>
2019-07-25 14:08:40 -0700

    fixes

--- testit.sh
+++ testit.sh
@@ -3,4 +3,15 @@ set -e
 
 source /opt/ros/kinetic/setup.bash
 cd /code
-catkin_make test
+if [ "$MAKE_ISOLATED" = "true" ]; then
+    echo "MAKING ISOLATED"
+    exit 1
+    catkin_make_isolated
+    catkin_make_isolated test
+    catkin_make_isolated install
+else
+    echo "MAKING NOT ISOLATED"
+    catkin_make
+    catkin_make test
+    catkin_make install
+fi
